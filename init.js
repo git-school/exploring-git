e8dea8989885a74260a98cf9869ec3f74be03ad4
Michelle Tilley <binarymuse@github.com>
2016-06-03 23:37:09 -0700

    Unzip things

--- init.js
+++ init.js
@@ -1,13 +1,23 @@
+import path from 'path'
 import fs from 'fs'
 
 import {s3} from 'aws'
+import {unzip} from 'zip'
 
 const AWS_KEY = 'abcdefg0123456789'
 s3.setKey(AWS_KEY)
 
 const BUCKET = 'my-s3-bucket'
 
+function extract(filename, buffer){
+  if(path.extname(filename) === 'zip'){
+    return unzip(buffer)
+  }else{
+    return bytes
+  }
+}
+
 function upload (filename, targetName) {
-  const bytes = fs.readFileSync(filename)
+  const bytes = extract(filenane, fs.readFileSync(filename))
   s3.putObject(BUCKET, filename, targetName)
 }
