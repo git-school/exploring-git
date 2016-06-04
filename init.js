aec6cbe89ed340e63eceaf94ce772a15ff2e1910
Michelle Tilley <binarymuse@github.com>
2016-06-03 23:30:33 -0700

    Pass target name to upload

--- init.js
+++ init.js
@@ -7,7 +7,7 @@ s3.setKey(AWS_KEY)
 
 const BUCKET = 'my-s3-bucket'
 
-function upload (filename) {
+function upload (filename, targetName) {
   const bytes = fs.readFileSync(filename)
-  s3.putObject(BUCKET, filename)
+  s3.putObject(BUCKET, filename, targetName)
 }
