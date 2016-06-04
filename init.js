607840ff1b141d13f3136f9e09b32f06e69af183
Michelle Tilley <binarymuse@github.com>
2016-06-03 23:40:21 -0700

    Keep sections more separate

--- init.js
+++ init.js
@@ -4,10 +4,12 @@ import fs from 'fs'
 import {s3} from 'aws'
 import {unzip} from 'zip'
 
+
 const AWS_KEY = 'abcdefg0123456789'
 s3.setKey(AWS_KEY)
 
 const BUCKET = 'my-s3-bucket'
+
 
 function extract (filename, buffer) {
   if (path.extname(filename) === 'zip') {
